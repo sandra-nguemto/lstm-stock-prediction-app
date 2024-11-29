227b74c2bfafc02510006cb71411220f7dc48c5c
Sandra Nguemto <agn2120@columbia.edu>
2024-11-29 16:07:46 -0500

    update configs

--- configs.py
+++ configs.py
@@ -6,8 +6,15 @@ env_path = Path('.') / 'api_key.env'
 
 load_dotenv(dotenv_path=env_path)
 
+
+import boto3
+secrets_manager = boto3.client('secretsmanager')
+secret_value = secrets_manager.get_secret_value(SecretId='ALPHA_VANTAGE_KEY')
+api_key = secret_value['SecretString']
+
+
 config = {"alpha_vantage": {
-        "key": os.getenv("ALPHA_VANTAGE_KEY"),
+        "key": 'api_key',
         "outputsize": "full",
         "key_adjusted_close": "4. close",
     },
