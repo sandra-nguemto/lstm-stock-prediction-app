52a9129080f7d9a04875938a923024e68e8d48e8
Sandra Nguemto <agn2120@columbia.edu>
2024-12-06 08:04:49 -0500

    update display

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -14,7 +14,13 @@ data = response.json()
 stock_data = data.get("outputs", {})
 # Display the data
 
+# for key, value in stock_data.items():
+#     st.write(f"**Stock**: {key}")
+#     st.write(f"**Predicted Price**: {value}")
+#     st.write("----")
+
 for key, value in stock_data.items():
-    st.write(f"**Stock**: {key}")
-    st.write(f"**Predicted Price**: {value}")
-    st.write("----")
+    col1, col2 = st.columns([2, 1])  # Create two columns with different widths
+    col1.markdown(f"### <span style='color:purple;'>**{key}** </span>", unsafe_allow_html=True)
+    col2.markdown(f"**Predicted Price**<span style='color:black;'>: {value}</span>", unsafe_allow_html=True)
+    st.write("----")  # Add a separator line
